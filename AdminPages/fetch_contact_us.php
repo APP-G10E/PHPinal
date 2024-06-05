ad112e0c632749f7d41b444dcc111701de513f46
un_ours_blanc <46861286+loursblancgithub@users.noreply.github.com>
2024-06-05 22:04:46 +0200

    contacts visibles par le super

---
+++ AdminPages/fetch_contact_us.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "app_g10e";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT firstName, surname, email, msg FROM contact_us";
+$result = $conn->query($sql);
+
+$data = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+?>
